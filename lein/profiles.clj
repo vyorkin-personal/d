101f943e91ccf7966d2f37c0ca0fe0bfdd1fcf90
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-03-06 01:27:29 +0300

    Add lein

---
+++ lein/profiles.clj
@@ -0,0 +1,21 @@
+{:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
+
+{:user {:plugins [[lein-dotenv "RELEASE"]
+                  [lein-exec "0.3.4"]
+                  [lein-pprint "1.1.1"]
+                  [lein-ancient "0.6.0-SNAPSHOT"]
+                  [lein-annotations "0.1.0"]
+                  [lein-autoreload "0.1.0"]
+                  [lein-bikeshed "0.2.0"]
+                  [lein-cprint "1.2.0"]
+                  [lein-ns-dep-graph "0.1.0-SNAPSHOT"]
+                  [lein-vanity "0.2.0"]
+                  [lein-ring "0.9.7"]
+                  [lein-cloudbees "1.0.5"]
+                  [org.clojure/tools.trace "0.7.9"]
+                  [venantius/ultra "0.4.0"]]
+        }
+ }
