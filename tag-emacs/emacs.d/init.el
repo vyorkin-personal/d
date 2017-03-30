1ab37048b3ff4d63c0c1149634f97d55478952a7
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-03-31 02:58:07 +0300

    Move nvm init to zshrc, update emacs config

--- tag-emacs/emacs.d/init.el
+++ tag-emacs/emacs.d/init.el
@@ -1,3 +1,4 @@
+;; - exec-path-from-shell
 ;; - evil
 ;; - color-theme
 ;; - sr-speedbar
@@ -27,6 +28,8 @@
 ;; - evil-magit
 ;; - evil-leader
 ;; - evil-org
+;; - base16-themes
+;; - sublime-themes
 ;;
 ;; - typescript-mode:
 ;;   Basic font-lock/syntax-highlighting and indentation.
@@ -36,19 +39,24 @@
 ;;   TypeScript Interactive Development Environment for Emacs.
 ;;   https://github.com/ananthakumaran/tide
 ;;
+;; - js2-mode
+;; - js2-refactor
+;; - json-mode
+;;
 ;; - idris-mode
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(add-to-list 'default-frame-alist '(font . "Source Code Pro 12"))
-(set-frame-font "Source Code Pro 12" nil t)
+(add-to-list 'default-frame-alist '(font . "Source Code Pro 14"))
+(set-frame-font "Source Code Pro 14" nil t)
 
 ;; disable *GNU Emacs* buffer on startup
 (setq inhibit-startup-screen t)
+
 ;; disable native fullscreen support
-(setq ns-use-native-fullscreen nil)
+;; (setq ns-use-native-fullscreen t)
 
 ;; instead of a splash screen,
 ;; let's start with the Bookmark List
@@ -62,8 +70,6 @@
 
 ;; highlight the current line in the buffer
 (global-hl-line-mode 1)
-;; TODO: fixme
-;; (set-face-background hl-line-face "#111111")
 
 ;; hide the fringe
 (set-fringe-style 0)
@@ -71,6 +77,12 @@
 ;; over-write selection to make things
 ;; slightly less uncomfortable to others
 (delete-selection-mode t)
+
+;; disable annoying warning message about environment variables setting
+(setq exec-path-from-shell-check-startup-files nil)
+;; make Emacs use the $PATH set up by the user's shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 ;; mode line ;;
 
@@ -98,7 +110,7 @@
 ;; automatically save place in each file
 (save-place-mode t)
 
-(add-hook 'prog-mode-hook #'highlight-indentation-mode)
+;; (add-hook 'prog-mode-hook #'highlight-indentation-mode)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook #'idle-highlight-mode)
 
@@ -112,10 +124,12 @@
 (setq ivy-height 14)
 (setq enable-recursive-minibuffers t)
 (setq ivy-count-format "(%d/%d) ")
+
 ;; enable fuzzy matching
-(setq ivy-re-builders-alist
-      '((ivy-switch-buffer . ivy--regex-plus)
-        (t . ivy--regex-fuzzy)))
+;; (setq ivy-re-builders-alist
+;;      '((ivy-switch-buffer . ivy--regex-plus)
+;;        (t . ivy--regex-fuzzy)))
+
 ;; omit ^ at the beginning of regexp
 (setq ivy-initial-inputs-alist nil)
 
@@ -155,8 +169,9 @@
   "d" 'delete-window
   "o" 'other-window
   "w" 'ace-window
+  "q" 'sr-speedbar-toggle
   "SPC" 'delete-other-windows
-  "TAB" 'sr-speedbar-toggle
+  "RET" 'writeroom-mode
   "a" 'counsel-projectile-switch-project
   "c" 'projectile-invalidate-cache
   "g" 'magit-status)
@@ -182,6 +197,9 @@
 (setq auto-save-list-file-name nil) ; disable .saves files
 (setq auto-save-default nil)        ; disable auto saving
 
+;; turn off alarms completely
+(setq ring-bell-function 'ignore)
+
 ;; use spaces instead of tabs everywhere
 (setq-default indent-tabs-mode nil)
 ;; y/n instead of yes/no
@@ -197,29 +215,29 @@
 (setq undo-tree-auto-save-history t)
 
 ;; sr-speedbar ;;
+
 (setq sr-speedbar-right-side nil)
 (setq speedbar-use-images nil)
 (setq speedbar-show-unknown-files t)
 (setq sr-speedbar-width 25)
-(make-face 'speedbar-face)
-(set-face-font 'speedbar-face "Source Code Pro 10")
-(setq speedbar-mode-hook '(lambda () (buffer-face-set 'speedbar-face)))
+
+;; (make-face 'speedbar-face)
+;; (set-face-font 'speedbar-face "Source Code Pro 12")
+;; (setq speedbar-mode-hook '(lambda () (buffer-face-set 'speedbar-face)))
 
 ;; theme
 
 ;; customize-themes ;;
 
 ;; (load-theme 'monochrome t)
-(load-theme 'quasi-monochrome t)
-
-(require 'color)
-(let ((bg (face-attribute 'default :background)))
-    (custom-set-faces
-     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face :background ,(color-lighten-name bg 24))))))
-     `(company-tooltip-common ((t (:inherit font-lock-constant-face)))))
+;; (load-theme 'quasi-monochrome t)
+;; (load-theme 'abyss t)
+;; (load-theme 'cyberpunk t)
+;; (load-theme 'afternoon t)
+;; (load-theme 'paganini t)
+(load-theme 'gotham t)
+;; (load-theme 'gruber-darker t)
+;; (load-theme 'base16-grayscale-dark)
 
 ;; color-theme ;;
 
@@ -266,7 +284,8 @@
 (setq alchemist-goto-elixir-source-dir "~/projects/github/elixir")
 (setq alchemist-goto-erlang-source-dir "/usr/local/Cellar/erlang/19.2.3")
 
-(add-to-list 'elixir-mode-hook (alchemist-mode +1))
+;; (add-to-list 'elixir-mode-hook (alchemist-mode +1))
+(add-hook 'elixir-mode-hook 'alchemist-mode)
 (add-hook 'elixir-mode-hook
 	  (lambda ()
 	    (when (and
@@ -307,6 +326,23 @@
 ;;    "i" 'alchemist-hex-info-at-point))
 ;; (advice-add 'alchemist-hex-mode :after #'alchemist-hex-local-keys)
 
+;; javascript, json ;;
+
+(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; enable minor mode for js refactoring
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+;; see: https://github.com/magnars/js2-refactor.el#refactorings
+(js2r-add-keybindings-with-prefix "C-c C-j")
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
 ;; typescript, tide ;;
 
 (defun setup-tide-mode ()
@@ -337,6 +373,9 @@
 ;; global hotkeys
 
 (global-set-key (kbd "<f11>") 'toggle-frame-fullscreen)
+
+(global-set-key (kbd "C-M-=") 'default-text-scale-increase)
+(global-set-key (kbd "C-M--") 'default-text-scale-decrease)
 
 (global-set-key (kbd "C-h") 'windmove-left)
 (global-set-key (kbd "C-l") 'windmove-right)
@@ -375,27 +414,57 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#C2C2C2" "#161616" "#252525" "#080808" "#0E0E0E" "#161616" "#080808" "#080808"])
+ '(ansi-term-color-vector
+   [unspecified "#3b3228" "#cb6077" "#beb55b" "#f4bc87" "#8ab3b5" "#a89bb9" "#8ab3b5" "#d0c8c6"])
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "7997e0765add4bfcdecb5ac3ee7f64bbb03018fb1ac5597c64ccca8c88b1262f" "d9850d120be9d94dd7ae69053630e89af8767c36b131a3aa7b06f14007a24656" "85e6bb2425cbfeed2f2b367246ad11a62fb0f6d525c157038a0d0eaaabc1bfee" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" "78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "ffe80c88e3129b2cddadaaf78263a7f896d833a77c96349052ad5b7753c0c5a5" "cbd8e65d2452dfaed789f79c92d230aa8bdf413601b261dbb1291fb88605110c" "df21cdadd3f0648e3106338649d9fea510121807c907e2fd15565dde6409d6e9" "4bf5c18667c48f2979ead0f0bdaaa12c2b52014a6abaa38558a207a65caeb8ad" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "50b64810ed1c36dfb72d74a61ae08e5869edc554102f20e078b21f84209c08d1" "f869a5d068a371532c82027cdf1feefdc5768757c78c48a7e0177e90651503ad" "986e7e8e428decd5df9e8548a3f3b42afc8176ce6171e69658ae083f3c06211c" "87d46d0ad89557c616d04bef34afd191234992c4eb955ff3c60c6aa3afc2e5cc" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" "8be07a2c1b3a7300860c7a65c0ad148be6d127671be04d3d2120f1ac541ac103" "aded4ec996e438a5e002439d58f09610b330bbc18f580c83ebaba026bbef6c82" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "cea3ec09c821b7eaf235882e6555c3ffa2fd23de92459751e18f26ad035d2142" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "50d07ab55e2b5322b2a8b13bc15ddf76d7f5985268833762c500a90e2a09e7aa" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "4a91a64af7ff1182ed04f7453bb5a4b0c3d82148d27db699df89a5f1d449e2a4" "12670281275ea7c1b42d0a548a584e23b9c4e1d2dabb747fd5e2d692bcd0d39b" "aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "93268bf5365f22c685550a3cbb8c687a1211e827edc76ce7be3c4bd764054bad" "73ad471d5ae9355a7fa28675014ae45a0589c14492f52c32a4e9b393fcc333fd" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "b8929cff63ffc759e436b0f0575d15a8ad7658932f4b2c99415f3dde09b32e97" "2a998a3b66a0a6068bcb8b53cd3b519d230dd1527b07232e54c8b9d84061d48d" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "9c4acf7b5801f25501f0db26ac3eee3dc263ed51afd01f9dcfda706a15234733" "85d609b07346d3220e7da1e0b87f66d11b2eeddad945cac775e80d2c1adb0066" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" "5a39d2a29906ab273f7900a2ae843e9aa29ed5d205873e1199af4c9ec921aaab" "840db7f67ce92c39deb38f38fbc5a990b8f89b0f47b77b96d98e4bf400ee590a" "a62f0662e6aa7b05d0b4493a8e245ab31492765561b08192df61c9d1c7e1ddee" "ffac21ab88a0f4603969a24b96993bd73a13fe0989db7ed76d94c305891fad64" "45a8b89e995faa5c69aa79920acff5d7cb14978fbf140cdd53621b09d782edcf" "5f99055206ed6a1b9958f7dd5eaa9f884f8b5a8678bd0c5e2622aced5c4a1be7" "fc7fd2530b82a722ceb5b211f9e732d15ad41d5306c011253a0ba43aaf93dccc" "cabc32838ccceea97404f6fcb7ce791c6e38491fd19baa0fcfb336dcc5f6e23c" "1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "92192ea8f0bf04421f5b245d906701abaa7bb3b0d2b3b14fca2ee5ebb1da38d8" "760ce657e710a77bcf6df51d97e51aae2ee7db1fba21bbad07aab0fa0f42f834" "34ed3e2fa4a1cb2ce7400c7f1a6c8f12931d8021435bad841fdc1192bd1cc7da" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "308eb04bfba22cd158a0927362d3a138653755eb7e82d626088df845c77b59c6" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "b51c2dda65e8e7e66ab1b06bc10b59e61c153b0cf928f296efab5a7574779fb6" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "3ba81769ce9babc42d1e28e97c0909d48399df252ce76dbaf81f93f1879b5357" "715fdcd387af7e963abca6765bd7c2b37e76154e65401cd8d86104f22dd88404" "1e67765ecb4e53df20a96fb708a8601f6d7c8f02edb09d16c838e465ebe7f51b" "6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" "63dd8ce36f352b92dbf4f80e912ac68216c1d7cf6ae98195e287fd7c7f7cb189" "23ccf46b0d05ae80ee0661b91a083427a6c61e7a260227d37e36833d862ccffc" "01761172c43bd017296de3dcd9760e20aa1d39dd4f3822060815f9a3a21f8170" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(fci-rule-color "#F0F0F0")
+ '(hl-paren-colors
+   (quote
+    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
+ '(ivy-count-format "(%d/%d) ")
+ '(ivy-height 14)
+ '(linum-format " %7i ")
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (flycheck-elixir idle-highlight-mode rainbow-delimiters highlight-indentation dired+ smart-mode-line-powerline-theme smart-mode-line darkroom writeroom-mode evil-anzu ace-window yoshi-theme tao-theme monochrome-theme quasi-monochrome-theme ivy-hydra counsel-projectile counsel ivy idris-mode projectile-ripgrep ripgrep multiple-cursors emmet-mode evil-org alchemist evil-magit magit web-mode tide sr-speedbar projectile evil eldoc-overlay-mode company color-theme))))
+    (json-mode js2-refactor js2-mode exec-path-from-shell heroku-theme gruber-darker-theme gotham-theme farmhouse-theme phoenix-dark-pink-theme cyberpunk-theme calmer-forest-theme sublime-themes base16-theme kooten-theme afternoon-theme abyss-theme arjen-grey-theme danneskjold-theme paganini-theme hamburg-theme default-text-scale flycheck-elixir idle-highlight-mode rainbow-delimiters highlight-indentation dired+ smart-mode-line-powerline-theme smart-mode-line darkroom writeroom-mode evil-anzu ace-window yoshi-theme monochrome-theme quasi-monochrome-theme ivy-hydra counsel-projectile counsel ivy idris-mode projectile-ripgrep ripgrep multiple-cursors emmet-mode evil-org alchemist evil-magit magit web-mode tide sr-speedbar projectile evil eldoc-overlay-mode company color-theme)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "goldenrod")
+     (60 . "#e7c547")
+     (80 . "DarkOliveGreen3")
+     (100 . "#70c0b1")
+     (120 . "DeepSkyBlue1")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "goldenrod")
+     (200 . "#e7c547")
+     (220 . "DarkOliveGreen3")
+     (240 . "#70c0b1")
+     (260 . "DeepSkyBlue1")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "goldenrod")
+     (340 . "#e7c547")
+     (360 . "DarkOliveGreen3"))))
+ '(vc-annotate-very-old-color "#161616")
+ '(window-divider-default-right-width 1)
+ '(window-divider-mode t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-scrollbar-bg ((t (:background "#191919"))))
- '(company-scrollbar-fg ((t (:background "#0c0c0c"))))
- '(company-tooltip ((t (:inherit default :background "#050505"))))
- '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- '(company-tooltip-selection ((t (:inherit font-lock-function-name-face :background "#3d3d3d"))))
- '(idle-highlight ((t (:inherit default :background "#111111" :foreground "#ffaa00"))))
- '(ivy-minibuffer-match-face-1 ((t (:background "#333333"))))
- '(ivy-minibuffer-match-face-2 ((t (:background "#444444" :weight bold))))
- '(swiper-line-face ((t (:inherit black :background "black")))))
-
+ )
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
 ;; End:
