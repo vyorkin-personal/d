46ad9ba137a828cef5fe37c8aa6a37a0ce87226f
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-08-30 18:26:10 +0300

    Fix alchemist keybindings

--- tag-emacs/emacs.d/init.el
+++ tag-emacs/emacs.d/init.el
@@ -1163,30 +1163,33 @@
      alchemist-goto-erlang-source-dir "/usr/local/Cellar/erlang/19.2.3")
     :config
     (general-evil-define-key 'normal 'alchemist-mode-map
+      :prefix "SPC"
       ;; elixir general key bindings
-      " t" 'alchemist-mix-test
-      " T" 'alchemist-project-run-tests-for-current-file
-      " ," 'alchemist-test-toggle-test-report-display
-      " h" 'alchemist-help-search-at-point
-      " H" 'alchemist-help
-      " a" 'alchemist-project-toggle-file-and-tests
-      " A" 'alchemist-project-toggle-file-and-tests-other-window
-      " m" 'alchemist-mix
+      "t" 'alchemist-mix-test
+      "T" 'alchemist-project-run-tests-for-current-file
+      "," 'alchemist-test-toggle-test-report-display
+      "h" 'alchemist-help-search-at-point
+      "H" 'alchemist-help
+      "a" 'alchemist-project-toggle-file-and-tests
+      "A" 'alchemist-project-toggle-file-and-tests-other-window
+      "m" 'alchemist-mix
       ;; elixir IEx-specific key bindings
-      " e" 'alchemist-iex-project-run
-      " r" 'alchemist-iex-reload-module)
+      "e" 'alchemist-iex-project-run
+      "r" 'alchemist-iex-reload-module)
     (general-evil-define-key 'visual 'alchemist-mode-map
-      " e" 'alchemist-iex-send-current-line
-      " E" 'alchemist-iex-send-current-line-and-go
-      " r" 'alchemist-iex-send-region
-      " R" 'alchemist-iex-send-region-and-go)
+      :prefix "SPC"
+      "e" 'alchemist-iex-send-current-line
+      "E" 'alchemist-iex-send-current-line-and-go
+      "r" 'alchemist-iex-send-region
+      "R" 'alchemist-iex-send-region-and-go)
     ;; elixir HEX-specific key bindings
     (general-evil-define-key 'normal 'alchemist-hex-mode-map
-      " i" 'alchemist-hex-info-at-point
-      " I" 'alchemist-hex-info
-      " r" 'alchemist-hex-releases-at-point
-      " R" 'alchemist-hex-releases
-      " f" 'alchemist-hex-search))
+      :prefix "SPC"
+      "i" 'alchemist-hex-info-at-point
+      "I" 'alchemist-hex-info
+      "r" 'alchemist-hex-releases-at-point
+      "R" 'alchemist-hex-releases
+      "f" 'alchemist-hex-search))
   :config
   (add-hook 'elixir-mode-hook 'alchemist-mode)
   (add-hook 'elixir-mode-hook
