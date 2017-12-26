4dd6415bb702dd6625612f39cf52d39926a7a514
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-12-26 17:23:53 +0300

    Update emacs config (purescript & haskell modes)

--- tag-emacs/emacs.d/init.el
+++ tag-emacs/emacs.d/init.el
@@ -760,8 +760,9 @@
   (setq
    undo-tree-auto-save-history t
    undo-tree-history-directory-alist `((".*" . "~/.emacs.d/tmp")))
-  (setq undo-tree-visualizer-diff t)
-  (setq undo-tree-visualizer-timestamps t)
+  ;; diff is slow as hell and it doesn't destroy the diff buffer automatically
+  ;; (setq undo-tree-visualizer-diff t)
+  ;; (setq undo-tree-visualizer-timestamps t)
   :config
   (undo-tree-mode)
   :diminish undo-tree-mode)
@@ -1332,13 +1333,30 @@
 (use-package dockerfile-mode)
 
 ;; purescript
-(use-package purescript-mode)
+
+;; I use my forked version https://github.com/vyorkin-forks/purescript-mode/tree/bugfix/conflicting-quail-rules
+;; with some conflicting quail rules disabled from the local path
+;; (and I have no idea how to use "use-package" to use package from a local path lol)
+
+(add-to-list 'load-path "~/projects/github/purescript-mode/")
+(require 'purescript-mode-autoloads)
+(add-to-list 'Info-default-directory-list "~/projects/github/purescript-mode/")
+
+(defun rc/purescript-emmet ()
+  (interactive)
+  (let ((start (point))
+        (end (save-excursion (beginning-of-line-text) (point))))
+    (call-process-region start end "purescript-emmet" t t)))
+
+(general-evil-define-key 'insert 'purescript-mode-map
+    "C-c C-e" 'rc/purescript-emmet)
 
 (use-package psc-ide
   :preface
   (defun rc/psc-ide/setup ()
     (setq-local evil-auto-indent nil)
     (psc-ide-mode)
+    (turn-on-purescript-unicode-input-method)
     (turn-on-purescript-indentation))
   :after purescript-mode
   :init
@@ -1433,9 +1451,12 @@
 (use-package haskell-mode
   :preface
   (defun rc/haskell-mode/setup ()
+    (set (make-local-variable 'company-backends)
+         (append '((company-capf company-dabbrev-code))
+                 company-backends))
+    (haskell-doc-mode)
     (setq-local evil-auto-indent nil)
-    (haskell-indent-mode)
-    (haskell-doc-mode))
+    (haskell-indent-mode))
   :commands (haskell-indent-mode
              interactive-haskell-mode
              haskell-doc-mode)
