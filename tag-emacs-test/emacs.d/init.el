d23523318f90bec9a8637aec15f8b64110b02bf2
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2018-02-19 16:32:32 +0300

    Update emacs config

--- tag-emacs-test/emacs.d/init.el
+++ tag-emacs-test/emacs.d/init.el
@@ -4,36 +4,6 @@
 (setq custom-file "~/.emacs-custom.el")
 (load custom-file)
 
-(add-to-list 'load-path (expand-file-name "features" user-emacs-directory))
-
-(defvar rc/gc-threshold-increased 402653184)
-(defvar rc/gc-percentage-increased 0.6)
-
-(defvar rc/gc-threshold-normal 16777216)
-(defvar rc/gc-percentage-normal 0.1)
-
-(defun rc/disable-gc ()
-  (setq
-    gc-cons-threshold rc/gc-threshold-increased
-    gc-cons-percentage rc/gc-percentage-increased))
-
-(defun rc/restore-gc ()
-  (setq
-    gc-cons-threshold rc/gc-threshold-normal
-    gc-cons-percentage rc/gc-percentage-normal))
-
-;; disable GC during startup
-(rc/disable-gc)
-;; don't keep gc-cons-threshold too high. It helps to stave off the GC while
-;; emacs starts up, but afterwards it causes stuttering and random freezes. So
-;; Reset it to a reasonable default.
-(add-hook 'after-init-hook #'rc/restore-gc)
-
-;; disable garbage collection in minibuffer
-;; see: http://tiny.cc/7wd7ay
-(add-hook 'minibuffer-setup-hook #'rc/disable-gc)
-(add-hook 'minibuffer-exit-hook #'rc/restore-gc)
-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
@@ -50,15 +20,6 @@
 (eval-when-compile
   (require 'use-package))
 
-(setq
-  ;; install missing packages by default
-  use-package-always-ensure t
-  use-package-verbose nil)
-
-;; enable with t if you prefer
-(defconst *spell-check-support-enabled* nil)
-(defconst *is-a-mac* (eq system-type 'darwin))
-
 (use-package evil
   :init
   (setq evil-want-integration nil)
@@ -66,10 +27,6 @@
   (evil-mode t))
 
 (use-package evil-collection
-  :init
-  (setq
-    evil-collection-setup-minibuffer t
-    evil-collection-company-use-tgn nil)
   :config
   (evil-collection-init))
 
