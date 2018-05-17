6fbb496887bb5de90f42f213c0efca04a14ec7fb
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2018-05-17 09:38:48 +0300

    Update emacs config

--- tag-emacs-next/emacs.d/features/init-nix.el
+++ tag-emacs-next/emacs.d/features/init-nix.el
@@ -1,7 +1,18 @@
 (require 'init-company)
 
-(use-package nix-mode
+(use-package nix-mode)
+
+(use-package nixos-options
+  :after nix-mode)
+
+(use-package nix-sandbox
+  :after nix-mode)
+
+(use-package company-nixos-options
+  :requires init-company
+  :after (nix-mode company)
   :config
+  config
   (add-to-list 'company-backends 'company-nixos-options))
 
 (provide 'init-nix)
